962b81007b35613db25a25ad32ca38ff749851cb
patriceleva <patrice.leva@gmail.com>
2015-08-04 10:30:46 +0200

    test polymer en cours

--- app/client/templates/home/home.js
+++ app/client/templates/home/home.js
@@ -18,14 +18,17 @@ Template.Home.helpers({
         // return 0, 0 if the location isn't ready
         return Geolocation.latLng() || { lat: 0, lng: 0 };
     },
-    error: Geolocation.error
+    error: Geolocation.error,
+    attributeObject: function() {
+        return { jsonData : Restaurants.find({})};
+    }
 });
 
 /*Tracker.autorun(function() {
-    console.log(Geolocation.currentLocation());
-    console.log(Geolocation.error());
-    console.log(Geolocation.latLng());
-});*/
+ console.log(Geolocation.currentLocation());
+ console.log(Geolocation.error());
+ console.log(Geolocation.latLng());
+ });*/
 
 /*****************************************************************************/
 /* Home: Lifecycle Hooks */
